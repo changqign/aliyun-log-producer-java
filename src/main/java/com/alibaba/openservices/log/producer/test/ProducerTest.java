a3432b69b9019a1d9b2dd3a371000273de8b25d0
beizhou.zz <beizhou.zz@alibaba-inc.com>
2016-05-19 20:10:12 +0800

    commit

--- src/main/java/com/alibaba/openservices/log/producer/test/ProducerTest.java
+++ src/main/java/com/alibaba/openservices/log/producer/test/ProducerTest.java
@@ -12,7 +12,8 @@ import com.aliyun.openservices.log.common.LogItem;
 import com.aliyun.openservices.log.exception.LogException;
 
 public class ProducerTest {
-	private final static int ThreadsCount = 25;
+	private final static int ThreadsCount = 1;
+
 	public static String RandomString(int length) {
 		String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 		Random random = new Random();
@@ -23,7 +24,16 @@ public class ProducerTest {
 		}
 		return buf.toString();
 	}
-
+	public static String RandomMD5() {
+		String str = "abcdef0123456789";
+		Random random = new Random();
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < 32; i++) {
+			int num = random.nextInt(16);
+			buf.append(str.charAt(num));
+		}
+		return buf.toString();
+	}
 	public static void LowLevelPutLogsTest() {
 		Client client = new Client("cn-shanghai-corp.sls.aliyuncs.com",
 				"K59AYAGHpf8w9X0J", "7hvlClY49eO4v5Nm2EOkUOoIOlK3MW");
@@ -45,7 +55,7 @@ public class ProducerTest {
 		while (true) {
 			try {
 				client.PutLogs("ali-log-service", "test-producer", "",
-						logGroups.get(random.nextInt(99)), null);
+						logGroups.get(random.nextInt(100)), null);
 			} catch (LogException e) {
 				e.printStackTrace();
 			}
@@ -75,8 +85,9 @@ public class ProducerTest {
 				public void run() {
 					while (true) {
 						try {
-							client.PutLogs("ali-log-service", "test-producer", "",
-									logGroups.get(random.nextInt(1000000)), null);
+							client.PutLogs("ali-log-service", "test-producer",
+									"", logGroups.get(random.nextInt(1000000)),
+									null);
 						} catch (LogException e) {
 							e.printStackTrace();
 						}
@@ -88,11 +99,17 @@ public class ProducerTest {
 		Thread.sleep(24 * 60 * 60 * 1000);
 	}
 
-	public static void ProducerTest() throws InterruptedException {
+	public static void ProducerTest(int mem) throws InterruptedException {
 		ProducerConfig producerConfig = new ProducerConfig();
+		producerConfig.memPoolSizeInByte = mem;
+		producerConfig.packageTimeoutInMS = 100;
+		producerConfig.shardHashUpdateIntervalInMS = 1000;
 		final LogProducer producer = new LogProducer(producerConfig);
 		producer.updateProjectConfig(new ProjectConfig("ali-log-service",
 				"cn-shanghai-corp.sls.aliyuncs.com", "K59AYAGHpf8w9X0J",
+				"7hvlClY49eO4v5Nm2EOkUOoIOlK3MW"));
+		producer.updateProjectConfig(new ProjectConfig("ali-log-beizhou",
+				"cn-hangzhou-corp.sls.aliyuncs.com", "K59AYAGHpf8w9X0J",
 				"7hvlClY49eO4v5Nm2EOkUOoIOlK3MW"));
 		final Vector<Vector<LogItem>> logGroups = new Vector<Vector<LogItem>>();
 		for (int i = 0; i < 100000; ++i) {
@@ -109,12 +126,12 @@ public class ProducerTest {
 		Thread[] threads = new Thread[ThreadsCount];
 		for (int i = 0; i < ThreadsCount; ++i) {
 			threads[i] = new Thread(null, new Runnable() {
-				Random random = new Random();
-
+				Random random1 = new Random(), random2 = new Random();
+				
 				public void run() {
 					while (true) {
-						producer.send("ali-log-service", "test-producer", "",
-								null, logGroups.get(random.nextInt(99999)),
+						producer.send(random2.nextInt(2) == 0 ? "ali-log-service" : "ali-log-beizhou", "test-producer", "",
+								RandomMD5(), null, logGroups.get(random1.nextInt(100000)),
 								new CallbackTest());
 					}
 				}
@@ -125,12 +142,15 @@ public class ProducerTest {
 	}
 
 	public static void main(String args[]) throws InterruptedException {
+		/*
 		if (args[0].compareTo("sdk") == 0) {
 			LowLevelPutLogsTest();
-		} else if(args[0].compareTo("producer") == 0){
-			ProducerTest();
-		} else if(args[0].compareTo("any") == 0){
+		} else if (args[0].compareTo("producer") == 0) {
+			ProducerTest(Integer.parseInt(args[1]));
+		} else if (args[0].compareTo("any") == 0) {
 			PutEveryLogsTest();
 		}
+		//*/
+		ProducerTest(100 * 1024 * 1024);
 	}
 }
